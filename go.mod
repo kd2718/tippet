33c4997e0006a80c3ec7866a891376f4fdd6bd40
kd2718 <korydonati@gmail.com>
2025-08-04 20:34:52 -0700

    learning postgres extract

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 require (
 	github.com/apache/arrow-go/v18 v18.3.1
 	github.com/jackc/pgx/v5 v5.7.5
+	github.com/lib/pq v1.10.9
 )
 
 require (
@@ -12,8 +13,6 @@ require (
 	github.com/google/flatbuffers v25.2.10+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
-	github.com/zeebo/xxh3 v1.0.2 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
 	golang.org/x/mod v0.24.0 // indirect
--- scrap/pg_scrap.go
+++ scrap/pg_scrap.go
@@ -3,40 +3,70 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"os"
 
-	_ "github.com/jackc/pgx/v5"
+	_ "github.com/lib/pq"
 )
 
-type Connector interface {
-	Connect()
-}
-
-type PostgresSource struct {
-	conn *sql.DB
-}
-
-func NewPostgresSource(postgresConnector *sql.DB) (PostgresSource, error) {
-	return PostgresSource{
-		conn: postgresConnector,
-	}, nil
-
-}
+const (
+	host = "localhost"
+	port = 5432
+	user = "kdonati"
+	// password = nil
+	dbname = "kdonati"
+)
 
 func main() {
-
-	dbcon, err := sql.Open("psql", "localhost:5432")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		fmt.Printf("Error opening DB connection: %v\n", err)
-		os.Exit(1)
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
 	}
 
-	pgs, err := NewPostgresSource(dbcon)
-	if err != nil {
-		println("Error setting up PostgresSource: %v", err)
-		os.Exit(1)
-	}
-	println("%v", pgs.conn)
-	println("finished!!")
+	db.Exec(`CREATE if not exists TABLE users (
+ 	id SERIAL PRIMARY KEY,
+  	age INT,
+  	first_name TEXT,
+  	last_name TEXT,
+  	email TEXT UNIQUE NOT NULL
+	);`)
 
+	/*
+		sqlStatement := `
+			INSERT INTO users (age, email, first_name, last_name)
+			VALUES (31, 'jon2@calhoun.io', 'Jonathan', 'Calhoun')`
+		_, err = db.Exec(sqlStatement)
+		if err != nil {
+			panic(err)
+		}
+	*/
+
+	rows, err := db.Query(`select * from users;`)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var age int
+		var first_name string
+		var last_name string
+		var email string
+		err = rows.Scan(&id, &age, &first_name, &last_name, &email)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Name: %s\n", first_name)
+	}
+	//fmt.Printf("out:\n%v\n", out.)
+
+	fmt.Println("Successfully connected!")
 }
