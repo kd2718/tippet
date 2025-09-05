bb1b05265cab9d30bfc93a18b60c6950db981650
kd2718 <korydonati@gmail.com>
2025-09-04 21:45:03 -0700

    issues with adbc

--- scrap/pg_scrap.go
+++ scrap/pg_scrap.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/jackc/pgx/v5/pgxpool"
-	_ "github.com/jackc/pgx/v5/pgxpool"
+	"github.com/apache/arrow-adbc/go/adbc"
+	"github.com/apache/arrow-adbc/go/adbc/drivermgr"
+	//"github.com/jackc/pgx/v5/pgxpool"
+	//_ "github.com/jackc/pgx/v5/pgxpool"
 )
 
 const (
@@ -27,6 +28,70 @@ type User struct {
 }
 
 func main() {
+	/*
+		for this i had to "brew install tomlplusplus"
+		then I had to set some env variables
+			export CGO_CPPFLAGS="-I$(brew --prefix tomlplusplus)/include"
+			export CGO_LDFLAGS="-L$(brew --prefix tomlplusplus)/lib"
+
+		I don't think these are needed after I build, but I am not sure
+	*/
+	var drv drivermgr.Driver
+	ctx := context.Background()
+	pgURL := fmt.Sprintf("postgres://%s@%s:%d/%s", user, host, port, dbname)
+	//alloc := memory.DefaultAllocator
+
+	// 2. Instantiate the ADBC PostgreSQL driver
+	// The driver requires the memory allocator.
+	//drv := postgres.NewDriver(alloc)
+	//pg := postgres.New()
+	db, err := drv.NewDatabase(map[string]string{
+		"driver":          "abc_driver_postgres",
+		adbc.OptionKeyURI: pgURL,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	cxn, err := db.Open(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cxn.Close()
+
+	stmnt, err := cxn.NewStatement()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmnt.Close()
+
+	query := "select * from users;"
+	if err := stmnt.SetSqlQuery(query); err != nil {
+		log.Fatal(err)
+	}
+
+	rdr, rows, err := stmnt.ExecuteQuery(ctx)
+	defer rdr.Release()
+
+	fmt.Printf("read %s rows\n", rows)
+
+	fmt.Printf("****\n")
+	rdr.Schema()
+
+	for rdr.Next() {
+		rec := rdr.Record()
+		fmt.Printf("%s", rec)
+	}
+
+	fmt.Printf("The End")
+
+}
+
+/*
+func main2() {
 
 	ctx := context.Background()
 	pgURL := fmt.Sprintf("postgres://%s@%s:%d/%s", user, host, port, dbname)
@@ -43,6 +108,9 @@ func main() {
 		os.Exit(1)
 	}
 	defer rows.Close()
+
+	fmt.Printf("desc: - %v", rows.FieldDescriptions())
+	rows.
 
 	var users []User
 	for rows.Next() {
@@ -67,3 +135,4 @@ func main() {
 	}
 
 }
+*/
